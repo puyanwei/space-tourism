fde1c29e27eb1f7fb04edbc1ec71e086fe41ab87
Puyan Wei <puyanwei@gmail.com>
2023-04-17 11:42:52 +0100

    ... - Tailwind integration with Storybook - Add color theme to SB

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,19 @@
-import type { StorybookConfig } from "@storybook/nextjs";
+import type { StorybookConfig } from "@storybook/nextjs"
 const config: StorybookConfig = {
   stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm",
+    {
+      name: "@storybook/addon-styling",
+      options: {
+        // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
+        // For more details on this addon's options.
+        postCss: true,
+      },
+    },
   ],
   framework: {
     name: "@storybook/nextjs",
@@ -13,5 +22,5 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
-};
-export default config;
+}
+export default config
---
+++ stories/1-atoms/Colors.stories.tsx
@@ -0,0 +1,22 @@
+// Button.stories.ts|tsx
+
+import type { Meta, StoryObj } from "@storybook/react"
+
+const meta = {
+  title: "Theme Colors",
+}
+
+export default meta
+
+export const ThemeColors = {
+  render: () => (
+    <div className="p-2 space-y-2 text-black bg-slate-200">
+      <h1>Space Black (#0b0d17)</h1>
+      <div className="w-12 h-12 bg-space-black" />
+      <h1>Space Light Blue (#d0d6f9)</h1>
+      <div className="w-12 h-12 bg-space-light-blue" />
+      <h1>Space White (#ffffff)</h1>
+      <div className="w-12 h-12 bg-space-white" />
+    </div>
+  ),
+}
