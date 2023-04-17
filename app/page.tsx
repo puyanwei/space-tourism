046268afb89c14cad60d467d90fa6914ad08d51f
Puyan Wei <puyanwei@gmail.com>
2023-04-17 10:33:35 +0100

    Add custom colors to tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,9 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       Space Tourism
+      <div className="bg-white text-space-black">Space black</div>
+      <div className="bg-white text-space-light-blue">Space light blue</div>
+      <div className="text-space-white">Space white</div>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        "space-black": "#0b0d17",
+        "space-light-blue": "#d0d6f9",
+        "space-white": "#ffffff",
       },
     },
+    plugins: [],
   },
-  plugins: [],
 }
