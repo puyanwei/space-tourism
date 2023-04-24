0465d87aba4b7b5758e5a433253dbeaff2f4791c
Puyan Wei <puyanwei@gmail.com>
2023-04-24 12:55:45 +0100

    Add vertical versionof ButtonSlider

--- components/1-atoms/ButtonSlider/ButtonSlider.stories.tsx
+++ components/1-atoms/ButtonSlider/ButtonSlider.stories.tsx
@@ -13,11 +13,21 @@ type Story = StoryObj<typeof ButtonSlider>
 export const ButtonSliders: Story = {
   render: () => (
     <div>
-      <StorybookHeading>Active/Hover/Idle</StorybookHeading>
+      <StorybookHeading className="text-black">Active/Hover/Idle</StorybookHeading>
       <div className="flex space-x-4 my-4">
-        <ButtonSlider active />
+        <ButtonSlider active={true} />
         <ButtonSlider className="!bg-space-gray" />
         <ButtonSlider />
+      </div>
+      <StorybookHeading className="text-black">Vertical Version</StorybookHeading>
+      <div className="flex flex-col space-y-4 my-4">
+        <ButtonSlider active={true} version="vertical">
+          1
+        </ButtonSlider>
+        <ButtonSlider className="!border-2" version="vertical">
+          2
+        </ButtonSlider>
+        <ButtonSlider version="vertical">3</ButtonSlider>
       </div>
     </div>
   ),
--- components/1-atoms/ButtonSlider/index.tsx
+++ components/1-atoms/ButtonSlider/index.tsx
@@ -1,14 +1,30 @@
 import { Component } from "@/shared/types"
 
+const styleMap = {
+  horizontal: {
+    default: `h-[15px] w-[15px] rounded-full hover:bg-space-gray`,
+    idle: `bg-space-dark-gray`,
+    active: `bg-white`,
+  },
+  vertical: {
+    default: `h-20 w-20 rounded-full bg-black border-[1px] border-white hover:border-2  hover:bg-black hover:text-white font-bellefair border-transparent text-2xl`,
+    idle: `text-white`,
+    active: `bg-white text-black`,
+  },
+}
 interface ButtonSliderProps extends Component {
   active?: boolean
+  version?: "horizontal" | "vertical"
+  children?: string
 }
 
-export function ButtonSlider({ className, active = false }: ButtonSliderProps) {
-  const activeStyle = active ? `bg-white` : `bg-space-dark-gray`
-  return (
-    <button
-      className={`h-[15px] w-[15px] rounded-full hover:bg-space-gray ${activeStyle} ${className}`}
-    ></button>
-  )
+export function ButtonSlider({
+  className,
+  active = false,
+  version = "horizontal",
+  children = "",
+}: ButtonSliderProps) {
+  const baseStyle = styleMap[version].default
+  const activeStyle = active ? styleMap[version].active : styleMap[version].idle
+  return <button className={`${baseStyle} ${activeStyle} ${className}`}>{children}</button>
 }
