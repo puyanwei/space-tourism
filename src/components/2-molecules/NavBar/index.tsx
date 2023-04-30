5131cdc1da3173bdb02ab1d9ab62b3223b9e5e0e
Puyan Wei <puyanwei@gmail.com>
2023-05-01 00:38:30 +0100

    Dirty way of dealing with reponsive menu logic

--- src/components/2-molecules/NavBar/index.tsx
+++ src/components/2-molecules/NavBar/index.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link"
-import { useEffect, useState } from "react"
-import { IconWrapper } from "@/components/1-atoms/Icons"
+import { useState } from "react"
 import { warnAndReturnNull } from "@/shared/helpers"
 import { Component, PageUrls } from "@/shared/types"
 import { useWindowSize } from "src/hooks/useWindowSize"
@@ -50,6 +49,30 @@ export function NavBar({ data, currentPath, className = "" }: NavBarProps) {
         </span>
       )}
       {isMenuOpen && (
+        <nav
+          className={`pt-[118px] font-primary text-base letter-spacing-[2.7px] uppercase text-white backdrop-blur-sm bg-space-gray/10 h-screen md:h-[90px] absolute right-0 top-0 w-[60%] z-0 ${className}`}
+        >
+          <ul className="flex flex-col">
+            {data.map(({ href, text }, index) => {
+              const navNumber = index < 10 ? `0${index}` : index
+              const resolvedText = `/${text.toLowerCase()}`
+              const resolvedCurrentPath = currentPath === "/" ? "/home" : currentPath
+              const linkStyle =
+                resolvedCurrentPath === resolvedText ? `border-white` : `border-transparent`
+
+              return (
+                <Link href={href} key={`${index}-${text}`}>
+                  <li className={`px-8 py-4  cursor-pointer border-r-[3px] ${linkStyle}`}>
+                    <b className="pr-2">{navNumber}</b>
+                    {text}
+                  </li>
+                </Link>
+              )
+            })}
+          </ul>
+        </nav>
+      )}
+      {!isMobileScreenSize && (
         <nav
           className={`font-primary text-base letter-spacing-[2.7px] uppercase text-white backdrop-blur-sm bg-space-gray/10 h-screen md:h-[90px] absolute right-0 top-0 md:top-10 w-[65%] md:px-20 z-0 ${className}`}
         >
