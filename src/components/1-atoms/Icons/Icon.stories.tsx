925134bbb7a5c2969154a2274685fa5eeb2c2172
Puyan Wei <puyanwei@gmail.com>
2023-05-03 10:55:07 +0100

    Updated Icons SB section with a thinner width

--- src/components/1-atoms/Icons/Icon.stories.tsx
+++ src/components/1-atoms/Icons/Icon.stories.tsx
@@ -12,7 +12,7 @@ export default meta
 
 export const Icons = {
   render: () => (
-    <div className="grid grid-cols-2 gap-2">
+    <div className="grid grid-cols-2 gap-2 max-w-sm">
       <StorybookHeading className="mb-0 text-left">Logo</StorybookHeading>
       <IconWrapper className="place-self-center" icon={<Logo />} />
       <StorybookHeading className="mb-0">Icon Close</StorybookHeading>
