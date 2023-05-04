5f91d71808fb47f09b69e838c679ad12ee2ce37f
Puyan Wei <puyanwei@gmail.com>
2023-05-04 13:08:37 +0100

    PlanetPanel component w/mock data

--- src/components/1-atoms/Text/index.tsx
+++ src/components/1-atoms/Text/index.tsx
@@ -8,7 +8,7 @@ interface TextProps extends Component {
 export function Text({ className = "", children, theme = "primary" }: TextProps) {
   return (
     <p
-      className={`font-primary text-[15px] md:text-[16px] lg:text-[18px] text-white leading-7 lg:leading-8 tracking-wider font-${theme} ${className}`}
+      className={`font-primary text-[15px] md:text-[16px] lg:text-[18px] text-space-light-blue leading-7 lg:leading-8 tracking-wider font-${theme} ${className}`}
     >
       {children}
     </p>
