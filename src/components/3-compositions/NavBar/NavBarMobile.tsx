e282f59803cb3f4a6df71c55c964eb6137a349f7
Puyan Wei <puyanwei@gmail.com>
2023-05-04 12:14:11 +0100

    Fix for mobile menu visual bug

--- src/components/3-compositions/NavBar/NavBarMobile.tsx
+++ src/components/3-compositions/NavBar/NavBarMobile.tsx
@@ -33,32 +33,33 @@ export function NavBarMobile({ data, currentPath, className = "" }: NavBarProps)
           </Button>
         )}
       </span>
+      <div className="fixed inset-0 overflow-hidden">
+        <nav
+          className={`pt-[118px] font-primary text-base letter-spacing-[2.7px] uppercase text-white backdrop-blur-2xl bg-space-gray/10 h-screen md:h-[90px] absolute right-0 top-0 w-[60%] z-0 transform ease-in-out duration-300 ${slideAnimation} ${className}`}
+        >
+          <ul className="flex flex-col">
+            {data.map(({ href, text }, index) => {
+              const navNumber = index < 10 ? `0${index}` : index
+              const resolvedText = `/${text.toLowerCase()}`
+              const resolvedCurrentPath = currentPath === "/" ? "/home" : currentPath
+              const linkStyle =
+                resolvedCurrentPath === resolvedText ? `border-white` : `border-transparent`
 
-      <nav
-        className={`pt-[118px] font-primary text-base letter-spacing-[2.7px] uppercase text-white backdrop-blur-sm bg-space-gray/10 h-screen md:h-[90px] absolute right-0 top-0 w-[60%] z-0 transform ease-in-out duration-300 ${slideAnimation} ${className}`}
-      >
-        <ul className="flex flex-col">
-          {data.map(({ href, text }, index) => {
-            const navNumber = index < 10 ? `0${index}` : index
-            const resolvedText = `/${text.toLowerCase()}`
-            const resolvedCurrentPath = currentPath === "/" ? "/home" : currentPath
-            const linkStyle =
-              resolvedCurrentPath === resolvedText ? `border-white` : `border-transparent`
-
-            return (
-              <Link href={href} key={`${index}-${text}`}>
-                <li
-                  className={`px-8 py-4  cursor-pointer border-r-[3px] ${linkStyle}`}
-                  onClick={() => setIsMenuOpen(false)}
-                >
-                  <b className="pr-2">{navNumber}</b>
-                  {text}
-                </li>
-              </Link>
-            )
-          })}
-        </ul>
-      </nav>
+              return (
+                <Link href={href} key={`${index}-${text}`}>
+                  <li
+                    className={`px-8 py-4  cursor-pointer border-r-[3px] ${linkStyle}`}
+                    onClick={() => setIsMenuOpen(false)}
+                  >
+                    <b className="pr-2">{navNumber}</b>
+                    {text}
+                  </li>
+                </Link>
+              )
+            })}
+          </ul>
+        </nav>
+      </div>
     </>
   )
 }
