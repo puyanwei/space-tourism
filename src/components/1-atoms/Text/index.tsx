c7c2e5cb889bdcc411136a5bac7cd75e4a63f208
Puyan Wei <puyanwei@gmail.com>
2023-05-06 17:40:51 +0100

    Better naming for font size override prop

--- src/components/1-atoms/Text/index.tsx
+++ src/components/1-atoms/Text/index.tsx
@@ -3,11 +3,11 @@ import { Component } from "@/shared/types"
 interface TextProps extends Component {
   children: string
   theme?: "primary" | "serif"
-  size?: string
+  sizeOverride?: string
 }
 
-export function Text({ className = "", children, theme = "primary", size }: TextProps) {
-  const resolvedSize = size || "text-[15px] md:text-[16px] lg:text-[18px]"
+export function Text({ className = "", children, theme = "primary", sizeOverride }: TextProps) {
+  const resolvedSize = sizeOverride || "text-[15px] md:text-[16px] lg:text-[18px]"
   return (
     <p
       className={`font-primary text-space-light-blue leading-7 lg:leading-8 tracking-wider ${resolvedSize} font-${theme} ${className}`}
--- src/components/2-molecules/PlanetStatistics/index.tsx
+++ src/components/2-molecules/PlanetStatistics/index.tsx
@@ -12,13 +12,13 @@ export function PlanetStatistics({ distance, travelTime }: PlanetStatisticsProps
       <Text className="order-1 md:order-none lg:text-sm py-2 md:py-0 ">Avg. Distance</Text>
       <Text className="order-3 md:order-none lg:text-sm py-2 md:py-0 ">Est. Travel Time</Text>
       <Text
-        className="order-2 md:order-none text-[32px] md:text-[28px] pb-4 md:py-0 text-white"
+        className="order-2 md:order-none pb-4 md:py-0 text-white"
         theme="serif"
-        size="text-[28px]"
+        sizeOverride="text-[28px]"
       >
         {distance}
       </Text>
-      <Text className="order-4 md:order-none text-white" theme="serif" size="text-[28px]">
+      <Text className="order-4 md:order-none text-white" theme="serif" sizeOverride="text-[28px]">
         {travelTime}
       </Text>
     </div>
