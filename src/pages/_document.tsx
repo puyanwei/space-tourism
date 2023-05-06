bf9122989b3b550b1eaf706a47d60a0fde9a28c3
Puyan Wei <puyanwei@gmail.com>
2023-05-06 13:35:54 +0100

    html tag should be full height not full viewport height

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document"
 
 export default function Document() {
   return (
-    <Html className="scroll-smooth scroll-pt-16 h-screen">
+    <Html className="scroll-smooth scroll-pt-16 h-full">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="use-credentials" />
