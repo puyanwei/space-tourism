a83fa1b26cb43122f194a29faf8f7f2d919288d2
Puyan Wei <puyanwei@gmail.com>
2023-05-07 22:53:53 +0100

    Add CrewPanel to page

--- src/pages/crew/index.tsx
+++ src/pages/crew/index.tsx
@@ -1,7 +1,24 @@
-export default function Crew() {
-  return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      Meet your Crew
-    </main>
-  )
+import { CrewPanel } from "@/components/3-compositions/CrewPanel"
+import { Data } from "@/shared/types"
+import fs from "fs"
+import { GetStaticProps } from "next"
+
+interface CrewProps {
+  data: Data
+}
+
+export const getStaticProps: GetStaticProps<CrewProps> = async () => {
+  const jsonData = fs.readFileSync("src/json/data.json").toString()
+  const data = (await JSON.parse(jsonData)) as Data
+
+  return {
+    props: {
+      data,
+    },
+  }
+}
+
+export default function Crew({ data }: CrewProps) {
+  const { crew } = data
+  return <CrewPanel data={crew} />
 }
