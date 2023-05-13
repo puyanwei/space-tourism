6c3a452ba29b1381f9ac7428c75a15b5a72aba47
Puyan Wei <puyanwei@gmail.com>
2023-05-13 14:22:52 +0100

    Add black background image for bg image component for pre loaded image

--- src/components/1-atoms/BackgroundImage/index.tsx
+++ src/components/1-atoms/BackgroundImage/index.tsx
@@ -8,7 +8,7 @@ interface BackgroundImageProps extends Component {
 export function BackgroundImage({ url }: BackgroundImageProps) {
   return (
     <Image
-      className="!fixed pointer-events-none -z-10 inset-0 object-cover"
+      className="!fixed pointer-events-none -z-10 inset-0 object-cover bg-black"
       src={url}
       alt="background image of space"
       fill
