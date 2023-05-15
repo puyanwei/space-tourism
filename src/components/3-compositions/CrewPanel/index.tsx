2b535a06e1516a2905829f897a3e15079c28468e
Puyan Wei <puyanwei@gmail.com>
2023-05-15 11:00:25 +0100

    Crew page image size fix

--- src/components/3-compositions/CrewPanel/index.tsx
+++ src/components/3-compositions/CrewPanel/index.tsx
@@ -34,8 +34,8 @@ export function CrewPanel({ data }: CrewPanalProps) {
   const tagSize = width > mobileSize ? "h3" : "h5"
 
   return (
-    <div className="flex flex-col col-span-full flex-wrap content-center md:content-normal -mb-6 lg:grid lg:grid-cols-12">
-      <div className="grid grid-cols-1 lg:col-span-7 md:flex md:flex-col">
+    <div className="flex flex-col col-span-full -mb-6 lg:grid lg:grid-cols-12 lg:justify-end">
+      <div className="lg:col-span-7 flex flex-col">
         <SpaceSubheading className="md:pl-16 lg:pl-0" number="02">
           Meet your crew
         </SpaceSubheading>
@@ -49,7 +49,7 @@ export function CrewPanel({ data }: CrewPanalProps) {
           />
         </div>
         <Text
-          className="uppercase  self-center lg:self-start md:py-4"
+          className="uppercase self-center lg:self-start lg:py-4"
           theme="serif"
           color="text-space-gray"
           sizeOverride="text-[16px] md:text-[24px] lg:text-[32px]"
@@ -57,12 +57,14 @@ export function CrewPanel({ data }: CrewPanalProps) {
           {role}
         </Text>
         <Heading
-          className="uppercase lg:leading-[50px] self-center lg:self-start md:pb-4"
+          className="uppercase lg:leading-[50px] self-center lg:self-start lg:pb-4"
           level={tagSize}
         >
           {name}
         </Heading>
-        <Text className="pt-4 px-6 lg:px-0 self-center lg:self-start lg:max-w-md">{bio}</Text>
+        <Text className="pt-4 md:pt-0 px-6 lg:px-0 self-center lg:self-start lg:max-w-md">
+          {bio}
+        </Text>
         <div className="hidden lg:show lg:flex lg:flex-grow" />
         <CrewSlider className="py-4 md:py-8 space-x-4 ">
           {data.map(({ name }, index) => (
@@ -74,9 +76,9 @@ export function CrewPanel({ data }: CrewPanalProps) {
           ))}
         </CrewSlider>
       </div>
-      <div className="hidden md:block relative md:w-[450px] md:h-[315px] lg:w-[568px] lg:h-[670px]  border-b-2 border-b-space-dark-gray md:border-b-transparent -z-10 mx-auto lg:col-start-8">
+      <div className="hidden md:block relative md:w-full md:h-full lg:w-[568px] lg:h-[670px]  border-b-2 border-b-space-dark-gray md:border-b-transparent -z-10 mx-auto lg:col-start-8">
         <NextImage
-          className="object-contain md:fixed md:top-10 lg:top-20 md:object-bottom"
+          className="object-contain md:fixed lg:top-20 md:object-bottom"
           src={webp}
           fallbackSrc={png}
           alt={`${name}, ${role}`}
